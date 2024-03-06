c514f09239bdbe25c4286f63a99fabc39a5f475a
Taruna Wahyudi <taruna.wahyudi@asetku.com>
2024-03-06 18:00:12 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/taruna/wahyudi/wisatabandung/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.taruna.wahyudi.wisatabandung
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/detail_screen.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:wisatabandung/model/tourism_place.dart';
+
+var informationTextStyle = const TextStyle(fontFamily: 'Oxygen');
+
+class DetailScreen extends StatelessWidget {
+  final TourismPlace place;
+  const DetailScreen({super.key, required this.place});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Stack(
+                children: <Widget>[
+                  Image.asset(place.imageAsset),
+                  SafeArea(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey,
+                        child: IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: const Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
+                            )
+                        ),
+                      ),
+                    ),
+                  )
+                ]
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 16.0),
+              child: Text(
+                place.name,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Staatliches',
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: [
+                      const Icon(Icons.calendar_today),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        place.openDays,
+                        style: informationTextStyle,
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Icon(Icons.access_time),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        place.openTime,
+                        style: informationTextStyle,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Icon(Icons.monetization_on),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        place.ticketPrice,
+                        style: informationTextStyle,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                place.description,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontFamily: 'Oxygen',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: place.imageUrls.map((url) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.network(url),
+                  );
+                }).toList()
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({super.key});
+
+  @override
+  FavoriteButtonState createState() => FavoriteButtonState();
+
+}
+
+class FavoriteButtonState extends State<FavoriteButton> {
+
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        icon: const Icon(Icons.favorite_border),
+        onPressed: () {
+          setState(() {
+            isFavorite = !isFavorite
+          });
+        },
+    );
+  }
+  
+}
